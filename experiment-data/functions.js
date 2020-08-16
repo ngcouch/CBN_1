42937e33b17b59eaafa1d4466a40178f87edf7b0
Nathan Couch <ngcouch@gmail.com>
2020-08-16 16:33:22 -0500

    Changed feedback stim

--- experiment-data/functions.js
+++ experiment-data/functions.js
@@ -7,15 +7,21 @@ function network_stim(A, B, relation = "causes") {
 
 function feedback_stim(A, B, relation, response, valence) {
 
+
+    var sentence = "</p>"
+    sentence += network_stim(A, B, relation)
+    sentence += " is " + valence
+    sentence += ".</p>"
+    
     if (response) {
 
-	return "<p><big>Correct!</big></p>"
+	return "<p><big>Correct!</big></p>" + sentence
 
     }
 
     else {
 
-	return	"<p><big>Incorrect!</big></p>"
+	return	"<p><big>Incorrect!</big></p>" + sentence
 
     }
 
--- experiment-data/trials.js
+++ experiment-data/trials.js
@@ -4,85 +4,85 @@ var introduction = {type: 'html-keyboard-response',
 			choices: [32]
 		       }
 
-
-    // Define a pair of probe and feedback trials
-    var trial_pair = {
-	timeline: [
-	    {type: 'html-keyboard-response',
-	     stimulus: function(){
-		 X = jsPsych.timelineVariable('X', true)
-		 Y = jsPsych.timelineVariable('Y', true)
-		 relation = jsPsych.timelineVariable('relation', true)
-		 return network_stim(X, Y, relation)
-	     },
-	     data: {label: "learning"},
-	     choices: [70, 74],
-	     // code the response as to whether it was correct
-	     on_finish: function(data) {
-
-		 var response = data.key_press
-		 var valence = jsPsych.timelineVariable('valence', true)
-
-		 if (response == 70 && valence==true) {
-
-		     cum_response.push(true)
-		     data.correct = true
-
-		 }
-
-		 else if (response == 74 && valence==false) {
-
-		     cum_response.push(true)
-		     data.correct = true
-
-		 }
-
-		 else {
-
-		     cum_response.push(false)
-		     data.correct = false
-
-		 }
-	     }
-	    },
-	     {type: 'html-keyboard-response',
-	      stimulus: function() {
-		  var X = jsPsych.timelineVariable('X', true)
-		  var Y = jsPsych.timelineVariable('Y', true)
-		  var relation = jsPsych.timelineVariable('relation', true)
-		  var response = jsPsych.data.get().last(1).values()[0].correct
-		  var valence = jsPsych.timelineVariable('valence', true)
-
-		  return feedback_stim(X, Y, relation, response, valence)
-	      },
-	      data: {label: "feedback"},
-	      trial_duration: function() {
-
-		  if(jsPsych.data.get().last(1).values()[0].correct) {
-
-		      return 500
-
-		  }
-		  else {
-
-		      return 1500
 		      
-		  }
+// Define a pair of probe and feedback trials
+var learning_trial = {
+    timeline: [
+	{type: 'html-keyboard-response',
+	 stimulus: function(){
+	     X = jsPsych.timelineVariable('X', true)
+	     Y = jsPsych.timelineVariable('Y', true)
+	     relation = jsPsych.timelineVariable('relation', true)
+	     return network_stim(X, Y, relation)
+	 },
+	 data: {label: "learning"},
+	 choices: [70, 74],
+	 // code the response as to whether it was correct
+	 on_finish: function(data) {
+	     
+	     var response = data.key_press
+	     var valence = jsPsych.timelineVariable('valence', true)
 
-	      },
-	      response_ends_trial: false,
-	      choices: jsPsych.NO_KEYS
+	     if (response == 70 && valence==true) {
+
+		 cum_response.push(true)
+		 data.correct = true
+
 	     }
-	    ]
-    }	  
 
-    // Set them up in a block
-    var block = {
-	timeline: [trial_pair],
-	timeline_variables: network,
-	randomize_order: true,
+	     else if (response == 74 && valence==false) {
 
-    }
+		 cum_response.push(true)
+		 data.correct = true
+
+	     }
+
+	     else {
+
+		 cum_response.push(false)
+		 data.correct = false
+
+	     }
+	 }
+	},
+	{type: 'html-keyboard-response',
+	 stimulus: function() {
+	     var X = jsPsych.timelineVariable('X', true)
+	     var Y = jsPsych.timelineVariable('Y', true)
+	     var relation = jsPsych.timelineVariable('relation', true)
+	     var response = jsPsych.data.get().last(1).values()[0].correct
+	     var valence = jsPsych.timelineVariable('valence', true)
+
+	     return feedback_stim(X, Y, relation, response, valence)
+	 },
+	 data: {label: "feedback"},
+	 trial_duration: function() {
+
+	     if(jsPsych.data.get().last(1).values()[0].correct) {
+
+		 return 500
+
+	     }
+	     else {
+
+		 return 1500
+		 
+	     }
+
+	 },
+	 response_ends_trial: false,
+	 choices: jsPsych.NO_KEYS
+	}
+    ]
+}	  
+
+// Set them up in a block
+var block = {
+    timeline: [learning_trial],
+    timeline_variables: network,
+    randomize_order: true,
+
+}
 
 // Loop over the blocks, only advancing to the test phase once criterion is reached
 var learning = {
